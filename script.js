d6bfd413ea393a551876010886cd3070b440ea58
Lucky-light-1729 <agrawalanay20@gmail.com>
2025-06-02 09:15:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+const textBox = document.getElementById("evaluatedText");
+const countDisplay = document.getElementById("letterCount");
+
+textBox.addEventListener("input", () => {
+  countDisplay.innerText = textBox.value.length;
+});
